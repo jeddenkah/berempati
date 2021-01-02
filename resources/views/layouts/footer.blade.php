dd113d416f1b5d2a33f5fe14e5bda4ceea3e8259
M. Yusril Rigaldi <jangucil73@gmail.com>
2021-01-03 02:59:59 +0700

    footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,13 +3,12 @@
         <div class="row align-items-center justify-content-xl-between">
             <div class="col-xl-6">
                 <div class="copyright text-center text-xl-left text-muted">
-                    &copy; 2020 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1"
-                        target="_blank">Creative Tim</a>
+                    &copy; 2020 <a href="{{route('home')}}" class="font-weight-bold ml-1">Berempati</a>
                 </div>
             </div>
             <div class="col-xl-6">
                 <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-                    <li class="nav-item">
+                    {{-- <li class="nav-item">
                         <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
                     </li>
                     <li class="nav-item">
@@ -22,7 +21,7 @@
                     <li class="nav-item">
                         <a href="https://github.com/creativetimofficial/argon-dashboard/blob/master/LICENSE.md"
                             class="nav-link" target="_blank">MIT License</a>
-                    </li>
+                    </li> --}}
                 </ul>
             </div>
         </div>
